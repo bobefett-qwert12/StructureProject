d52ff6ec34a60c5b954b2995fba689bc9cd53dcf
RyanPerkins <bobefett.qwert12@gmail.com>
2019-01-28 09:17:48 -0700

    added a runner and a node header

---
+++ StructureProject/Model/Node.hpp
@@ -0,0 +1,27 @@
+//
+//  Node.hpp
+//  StructureProject
+//
+//  Created by Perkins, Ryan on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <assert.h>
+
+template <class Type>
+class Node
+{
+private:
+    Type data;
+public :
+    Node(Type data);
+    Type getData();
+    void setData(Type data);
+};
+
+//Template definitions
+
+#endif /* Node_h */
