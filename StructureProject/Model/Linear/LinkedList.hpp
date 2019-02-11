a84ae1ed8b2e542edf0b6d633aa8c6b7235d4419
RyanPerkins <bobefett.qwert12@gmail.com>
2019-02-11 09:24:17 -0700

    implemented the Stack.hpp class

--- StructureProject/Model/Linear/LinkedList.hpp
+++ StructureProject/Model/Linear/LinkedList.hpp
@@ -24,10 +24,10 @@ public:
     int getSize() const;
     LinearNode<Type> * getFront();
     LinearNode<Type> * getEnd();
-    void add(Type item);
-    void addAtIndex(int index, Type item);
-    Type getFromIndex(int index);
-    Type remove(int index);
+    virtual void add(Type item);
+    virtual void addAtIndex(int index, Type item);
+    virtual Type getFromIndex(int index);
+    virtual Type remove(int index);
     //Type setAtIndex(int index, Type item);
     bool contains(Type item);
 };
