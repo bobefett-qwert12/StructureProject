5f5f198fc0b67dfd3eb32f5328e070bda2139fdd
RyanPerkins <bobefett.qwert12@gmail.com>
2019-02-13 09:16:54 -0700

    added music and crimeData implementation, along with fileController

---
+++ StructureProject/Controller/FileController.cpp
@@ -0,0 +1,9 @@
+//
+//  FileController.cpp
+//  StructureProject
+//
+//  Created by Perkins, Ryan on 2/13/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "FileController.hpp"
--- StructureProject/Model/Linear/Array.hpp
+++ StructureProject/Model/Linear/Array.hpp
@@ -30,7 +30,7 @@ public:
     int getSize() const;
     Type getFromIndex(int index);
     void setAtIndex(int index, Type data);
-}
+};
 
 template <class Type>
 Array<Type> :: Array(int size)
